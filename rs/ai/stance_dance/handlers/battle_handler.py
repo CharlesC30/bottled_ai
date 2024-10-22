fde23ce2eccaf9ee841cf432f14e25419175dc00
CharlesC30 <CharlesRClark30@gmail.com>
2024-10-22 11:01:38 -0400

    refactor infinite battling as function

--- rs/ai/stance_dance/handlers/battle_handler.py
+++ rs/ai/stance_dance/handlers/battle_handler.py
@@ -23,68 +23,62 @@ from rs.helper.logger import log
 # get_calm_enablers(state: GameState) -> List[str]
 # return appropriate enablers based on state
 
-class InfiniteBattler:
-    # def __init__(self, starting_state: GameState) -> None:
-    #     self.starting_state = starting_state
-    #     self.stance = StanceType.CALM if starting_state.has_relic("Tear Drop Locket") else StanceType.NO_STANCE
-    def __init__(self) -> None:
-        self.stance = None
 
-    def battle(self, state: GameState) -> Play:
-        played_rushdown = "rushdown" in [power['name'].lower() for power in state.get_player_combat()["powers"]]
-        all_cards_in_hand = (len(state.discard_pile.cards) == 0) and (len(state.draw_pile.cards) == 0)
-        cards_in_hand = [card.name for card in state.hand.cards]
-        if played_rushdown and all_cards_in_hand:
-            if self.stance is not StanceType.CALM:
-                # Find 'best' calm enabler and play
-                if "Inner Peace" in cards_in_hand:
-                    enabler = "Inner Peace"
-                elif "Inner Peace+" in cards_in_hand:
-                    enabler = "Inner Peace+"
-                elif any([("ATTACK" in monster['intent']) for monster in state.get_monsters()]):
-                    if "Fear No Evil" in cards_in_hand:
-                        enabler = "Fear No Evil"
-                    elif "Fear No Evil+" in cards_in_hand:
-                        enabler = "Fear No Evil+"
-                elif state.has_relic("Violet Lotus") and state.get_player_combat()["energy"] > 2:
-                    if "Vigilance" in cards_in_hand:
-                        enabler = "Vigilance"
-                    elif "Vigilance+" in cards_in_hand:
-                        enabler = "Vigilance+"
+def find_next_infinite_move(state: GameState, stance: StanceType) -> Play:
+    played_rushdown = "rushdown" in [power['name'].lower() for power in state.get_player_combat()["powers"]]
+    all_cards_in_hand = (len(state.discard_pile.cards) == 0) and (len(state.draw_pile.cards) == 0)
+    cards_in_hand = [card.name for card in state.hand.cards]
+    if played_rushdown and all_cards_in_hand:
+        if stance is not StanceType.CALM:
+            # Find 'best' calm enabler and play
+            if "Inner Peace" in cards_in_hand:
+                enabler = "Inner Peace"
+            elif "Inner Peace+" in cards_in_hand:
+                enabler = "Inner Peace+"
+            elif any([("ATTACK" in monster['intent']) for monster in state.get_monsters()]):
+                if "Fear No Evil" in cards_in_hand:
+                    enabler = "Fear No Evil"
+                elif "Fear No Evil+" in cards_in_hand:
+                    enabler = "Fear No Evil+"
+            elif state.has_relic("Violet Lotus") and state.get_player_combat()["energy"] > 2:
+                if "Vigilance" in cards_in_hand:
+                    enabler = "Vigilance"
+                elif "Vigilance+" in cards_in_hand:
+                    enabler = "Vigilance+"
 
-                self.stance = StanceType.CALM
-                card_index = state.hand.get_card_index_from_name(enabler)
-                if enabler == "Fear No Evil" or enabler == "Fear No Evil+":
-                    # find the index of first attacking monster
-                    monster_intents = [monster["intent"] for monster in state.get_monsters()]
-                    target_index = next((i for i, intent in enumerate(monster_intents) if "ATTACK" in intent), None)
-                    return (card_index, target_index)
-                return (card_index, -1)
-            
-        elif played_rushdown and self.stance is StanceType.CALM:
-            # Find and play wrath enabler 
-            if "Eruption+" in cards_in_hand:
-                enabler = "Eruption+"
-            elif "Tantrum" in cards_in_hand:
-                enabler = "Tantrum"
-            elif "Tantrum+" in cards_in_hand:
-                enabler = "Tantrum+"
-
-            self.stance = StanceType.WRATH
             card_index = state.hand.get_card_index_from_name(enabler)
-            target_index = random.randint(0, len([monster for monster in state.get_monsters() if not monster["is_gone"]]) - 1)
-            return (card_index, target_index)
-        
-        elif "Rushdown" in cards_in_hand:
-            card_index = state.hand.get_card_index_from_name("Rushdown")
+            if enabler == "Fear No Evil" or enabler == "Fear No Evil+":
+                # find the index of first attacking monster
+                monster_intents = [monster["intent"] for monster in state.get_monsters()]
+                target_index = next((i for i, intent in enumerate(monster_intents) if "ATTACK" in intent), None)
+                return (card_index, target_index)
             return (card_index, -1)
         
-        elif "Rushdown+" in cards_in_hand:
-            card_index = state.hand.get_card_index_from_name("Rushdown+")
-            return (card_index, -1)
-        
-        else:
-            return None
+    elif played_rushdown and stance is StanceType.CALM:
+        # Find and play wrath enabler 
+        if "Eruption+" in cards_in_hand:
+            enabler = "Eruption+"
+        elif "Tantrum" in cards_in_hand:
+            enabler = "Tantrum"
+        elif "Tantrum+" in cards_in_hand:
+            enabler = "Tantrum+"
+
+        stance = StanceType.WRATH
+        card_index = state.hand.get_card_index_from_name(enabler)
+        # attack the first alive monster
+        target_index = next((i for i, monster in enumerate(state.get_monsters()) if not monster["is_gone"]))
+        return (card_index, target_index)
+    
+    elif "Rushdown" in cards_in_hand:
+        card_index = state.hand.get_card_index_from_name("Rushdown")
+        return (card_index, -1)
+    
+    elif "Rushdown+" in cards_in_hand:
+        card_index = state.hand.get_card_index_from_name("Rushdown+")
+        return (card_index, -1)
+    
+    else:
+        return None
 
 
 class BattleHandler(Handler):
@@ -92,7 +86,6 @@ class BattleHandler(Handler):
     def __init__(self, config: BattleHandlerConfig = BattleHandlerConfig(), max_path_count: int = 11_000):
         self.config: BattleHandlerConfig = config
         self.max_path_count: int = max_path_count
-        self.infinite_battler = InfiniteBattler()
         self.in_infinite = False
 
     def can_handle(self, state: GameState) -> bool:
@@ -140,6 +133,7 @@ class BattleHandler(Handler):
             return self.config.transient_comparator()
         return self.config.general_comparator()
     
+    #TODO Refactor this to check for infinite in hand
     def has_infinite_in_deck(self, state: GameState) -> bool:
         # can add nuance later
         max_deck_size = 5        
@@ -166,21 +160,19 @@ class BattleHandler(Handler):
             if not self.in_infinite:
                 log("Starting Infinite")
                 self.in_infinite = True
-                self.infinite_battler.stance = StanceType.CALM if state.has_relic("Tear Drop Locket") else StanceType.NO_STANCE
-            next_move = self.infinite_battler.battle(state)
+            battle_state = create_battle_state(state)
+            next_move = find_next_infinite_move(state, stance=battle_state.get_stance())
 
-            # create a temp state for finding the state of the custom state after the chosen action
-            _state = create_battle_state(state)
-            _state.transform_from_play(next_move, is_first_play=False)  # not sure if it's ok that I'm setting that false
-            memory_book = TheBotsMemoryBook(memory_by_card=_state.memory_by_card.copy(), memory_general=_state.memory_general.copy())
+            battle_state.transform_from_play(next_move, is_first_play=False)
+            memory_book = TheBotsMemoryBook(memory_by_card=battle_state.memory_by_card.copy(), memory_general=battle_state.memory_general.copy())
+            
             if next_move[1] == -1:
                 return HandlerAction(commands=[f"play {next_move[0] + 1}"], memory_book=memory_book)
             return HandlerAction(commands=[f"play {next_move[0] + 1} {next_move[1]}"], memory_book=memory_book)
 
         actions = get_best_battle_action(state, self.select_comparator(state), self.max_path_count)
         if actions:
-            for command in actions.commands:
-                log(command)
+            log("Not using infinite")
             return actions
         if state.has_command(Command.END):
             self.in_infinite = False
